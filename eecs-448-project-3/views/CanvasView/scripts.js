5603eb12a06c15f33d937d949ca78b7da38ceee1
Max Patiiuk <max@patii.uk>
2021-10-16 22:39:53 -0500

    Integrate CanvasView with Person, Grid and Controls components

--- eecs-448-project-3/views/CanvasView/scripts.js
+++ eecs-448-project-3/views/CanvasView/scripts.js
@@ -4,7 +4,7 @@
  * New CanvasView(options).render(this.container)
  */
 /**
- * Base GameBoard class
+ * Base CanvasView class
  * @class CanvasView
  * @constructor
  * @param options
@@ -14,10 +14,19 @@
 class CanvasView extends View {
   #canvas;
 
-  #context;
+  #grid;
+
+  #cellSize;
+
+  #cellSizeUpdateListeners;
+
+  #player;
+
+  #controls;
 
   constructor() {
     super({});
+    this.#cellSizeUpdateListeners = [];
   }
 
   /**
@@ -34,8 +43,20 @@ class CanvasView extends View {
   ) {
     await super.render(container);
 
+    const overlay = this.container.getElementsByClassName('players')[0];
+    const playerContainer = document.createElement('div');
+    overlay.append(playerContainer);
+    this.#player = new Person();
+    await this.#player.render(playerContainer);
+    this.destructors.push(() => this.#player.remove());
+
     this.#canvas = this.container.getElementsByTagName('canvas')[0];
-    this.#context = this.#canvas.getContext('2d');
+    this.#grid = new Grid({ canvas: this.#canvas });
+    await this.#grid.render();
+    this.destructors.push(() => this.#grid.remove());
+    this.#cellSizeUpdateListeners.push(
+      this.#grid.handleCellResize.bind(this.#grid)
+    );
 
     const handleResize = this.handleResize.bind(this);
     window.addEventListener('resize', handleResize);
@@ -44,7 +65,11 @@ class CanvasView extends View {
     );
     handleResize();
 
-    // TODO: cell size: Math.ceil(Math.max(window.innerHeight,window.innerWidth)*0.04)
+    this.#controls = new Controls({
+      processKeyPress: this.#grid.handleKeyPress.bind(this),
+    });
+    await this.#controls.render();
+    this.destructors.push(() => this.#controls.remove());
 
     return this;
   }
@@ -52,5 +77,20 @@ class CanvasView extends View {
   handleResize() {
     this.#canvas.width = window.innerWidth;
     this.#canvas.height = window.innerHeight;
+    this.#cellSize = Math.ceil(
+      Math.max(window.innerHeight, window.innerWidth) * CELL_SIZE
+    );
+    this.container.style.setProperty('--cell-size', `${this.#cellSize}px`);
+
+    if (DEBUG)
+      console.log(
+        `Canvas size: ${this.#canvas.width}x${this.#canvas.height}.\t` +
+          `Cell size: ${this.#cellSize}px`
+      );
+
+    // Notify subscribed components about cell size change
+    this.#cellSizeUpdateListeners.forEach((listener) =>
+      listener(this.#cellSize)
+    );
   }
 }
