9a2ee00552bafb8bdbc116bd77de8b2a9b16a0d9
Max Patiiuk <max@patii.uk>
2021-10-16 22:38:20 -0500

    Add a Person component

---
+++ eecs-448-project-3/components/Person/scripts.js
@@ -0,0 +1,42 @@
+/*
+ * The name of this view
+ * Later, to render this view, call:
+ * New Person(options).render(this.container)
+ */
+/**
+ * Draw current player / NPC
+ * @class Person
+ * @constructor
+ * @param options
+ * @extends Component
+ * @public
+ */
+class Person extends Component {
+  #person;
+
+  constructor() {
+    super({});
+  }
+
+  /**
+   * @async
+   * @function render
+   * @memberof Person
+   * @param container Container to render the view within
+   */
+  async render(
+    // Container would be populated with elements from index.html
+    container
+  ) {
+    await super.render(container);
+
+    /*
+     * TODO: create an image for the player (from 4 directions)
+     *       and display it here using css background-image
+     */
+
+    this.#person = this.container.getElementsByClassName('person')[0];
+
+    return this;
+  }
+}
